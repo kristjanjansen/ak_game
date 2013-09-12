f62d8f59b95707f978d3e9aa0e5d9144b93cfcbd
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-09-12 16:02:36 +0300

    simpler

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 
 for i in $(find source/sprite -mindepth 1 -maxdepth 1 -type d)
 do
-  montage $i/*.png -geometry +0+0 -tile x1 -filter box -resize 800%x800% ${i//source/target}.png
+  montage $i/*.png -geometry +0+0 -tile x1 -filter box -resize 400%x400% ${i//source/target}.png
 done
 
 #montage source/tiles/*.png -geometry +0+0 -tile 1x -filter box -resize 400%x400% target/tiles/tiles.png 
--- public/js/entities/entities.js
+++ public/js/entities/entities.js
@@ -19,7 +19,7 @@ game.PlayerEntity = me.ObjectEntity.extend(
 		this.setVelocity(3, 15);
 	 
 		// adjust the bounding box
-		this.updateColRect(8,48, -1,0);
+//		this.updateColRect(8,48, -1,0);
 		
 		// set the display to follow our position on both axis
 		me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
