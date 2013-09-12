1fbfb282e89bb1227617c069b45e2777c54e5298
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-09-12 16:42:37 +0300

    ok

--- public/js/resources.js
+++ public/js/resources.js
@@ -6,6 +6,7 @@ game.resources = [
 	{name: "area01_level_tiles",  type:"image",	src: "data/img/map/area01_level_tiles.png"},
 	// the main player spritesheet
 	{name: "emo",     type:"image",	src: "data/img/sprite/emo.png"},
+	{name: "bodybuilder",     type:"image",	src: "data/img/sprite/bodybuilder.png"},
 	// the parallax background
 	{name: "area01_bkg0",         type:"image",	src: "data/img/area01_bkg0.png"},
 	{name: "area01_bkg1",         type:"image",	src: "data/img/area01_bkg1.png"},
