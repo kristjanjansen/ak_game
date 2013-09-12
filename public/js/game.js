2ba3dcf65e2589938ab728f1c51b21fe8c7dea49
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-09-12 15:49:14 +0300

    fixes

--- public/js/game.js
+++ public/js/game.js
@@ -42,7 +42,8 @@ var game = {
 		// enable the keyboard
 		me.input.bindKey(me.input.KEY.LEFT,		"left");
 		me.input.bindKey(me.input.KEY.RIGHT,	"right");
-		me.input.bindKey(me.input.KEY.X,		"jump", true);
+		me.input.bindKey(me.input.KEY.SPACE,	"jump", true);
+		me.input.bindKey(me.input.KEY.UP,	"jump", true);
 
         // Start the game.
         me.state.change(me.state.PLAY);
