b654f2f57b1c6e046bf80c3f45350bee08e51062
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-09-13 07:42:40 +0300

    reorg

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@
 
 #cp -R /Volumes/Disain\'s\ Public\ Folder/MK-11/game/sprite/* source/sprite/.
 
-for i in $(find source/sprite -mindepth 1 -maxdepth 1 -type d)
+rm -f public/data/img/sprite/*
+
+for i in $(find source/img/sprite -mindepth 1 -maxdepth 1 -type d)
 do
-  montage $i/*.png -geometry +0+0 -tile x1 -filter box -resize 400%x400% ${i//source/target}.png
+  montage $i/*.png -geometry +0+0 -tile x1 -filter box -resize 400%x400% ${i//source/public/data}.png
 done
 
 #montage source/tiles/*.png -geometry +0+0 -tile 1x -filter box -resize 400%x400% target/tiles/tiles.png 
 
-cp -R target/sprite/* public/data/img/sprite/.
+#cp -R target/sprite/* public/data/img/sprite/.
