6ce5cd01eb6a4e439c5764cf45cdc7fbf45e41e2
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-09-13 13:30:40 +0300

    reorg some more

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
+
 # http://superuser.com/questions/290656/combine-multiple-images-using-imagemagick
-
 #cp -R /Volumes/Disain\'s\ Public\ Folder/MK-11/game/sprite/* source/sprite/.
+#cp -R target/sprite/* public/data/img/sprite/.
 
-rm -f public/data/img/sprite/*
 
-for i in $(find source/img/sprite -mindepth 1 -maxdepth 1 -type d)
+# Sprites
+
+rm -f public/data/images/sprites/*
+
+for i in $(find source/images/sprites -mindepth 1 -maxdepth 1 -type d)
 do
   montage $i/*.png -geometry +0+0 -tile x1 -filter box -resize 400%x400% ${i//source/public/data}.png
 done
@@ -13,19 +17,21 @@ done
 
 # Background images
 
-rm -f public/data/img/background/*
+rm -f public/data/images/backgrounds/*
 
-for i in $(find source/img/background -mindepth 1 -maxdepth 1 -type f)
+for i in $(find source/images/backgrounds -mindepth 1 -maxdepth 1 -type f)
 do
   convert ${i} -filter box -resize 400%x400% ${i//source/public/data}
 done
 
 
-for i in $(find source/img/map -mindepth 1 -maxdepth 1 -type d)
+# Tiles
+
+rm -f public/data/images/tiles/*
+
+for i in $(find source/images/tiles -mindepth 1 -maxdepth 1 -type d)
 do
   montage $i/*.png -geometry +0+0 -tile 1x -filter box -resize 400%x400% ${i//source/public/data}.png
 done
 
-#montage source/tiles/*.png -geometry +0+0 -tile 1x -filter box -resize 400%x400% target/tiles/tiles.png 
 
-#cp -R target/sprite/* public/data/img/sprite/.
--- public/js/resources.js
+++ public/js/resources.js
@@ -1,14 +1,14 @@
 game.resources = [
 
-	{name: "tiles",  type:"image",	src: "data/img/map/tiles.png"},
+	{name: "tiles",  type:"image",	src: "data/images/tiles/tiles.png"},
 
-	{name: "emo", type:"image",	src: "data/img/sprite/emo.png"},
-	{name: "bodybuilder", type:"image",	src: "data/img/sprite/bodybuilder.png"},
-	{name: "doggie", type:"image",	src: "data/img/sprite/doggie.png"},
+	{name: "emo", type:"image",	src: "data/images/sprites/emo.png"},
+	{name: "bodybuilder", type:"image",	src: "data/images/sprites/bodybuilder.png"},
+	{name: "doggie", type:"image",	src: "data/images/sprites/doggie.png"},
 
-	{name: "background1", type:"image",	src: "data/img/background/background1.png"},
-	{name: "background2", type:"image",	src: "data/img/background/background2.png"},
+	{name: "background1", type:"image",	src: "data/images/backgrounds/background1.png"},
+	{name: "background2", type:"image",	src: "data/images/backgrounds/background2.png"},
 	
-	{name: "level1",  type: "tmx",	src: "data/map/level1.tmx"}
+	{name: "level1",  type: "tmx",	src: "data/levels/level1.tmx"}
 
 ];
