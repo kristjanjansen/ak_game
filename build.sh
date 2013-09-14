07e58ed2ae60cf4d88b44aba572c0e407397cecc
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-09-14 21:36:20 +0300

    wow

--- build.sh
+++ build.sh
@@ -9,11 +9,19 @@
 
 rm -f public/data/images/sprites/*
 
-for i in $(find source/images/sprites -mindepth 1 -maxdepth 1 -type d)
-do
-  montage $i/*.png -geometry +0+0 -tile x1 -filter box -resize 400%x400% ${i//source/public/data}.png
-done
+#for i in $(find source/images/sprites -mindepth 1 -maxdepth 1 -type d)
+#do
+#  montage $i/*.png -geometry +0+0 -tile x1 -filter box -resize 400%x400% ${i//source/public/data}.png
+#done
 
+for i in $(find source/images/sprites -name "*.psd" -mindepth 1 -maxdepth 1 -type f)
+do
+  convert $i -set dispose Background -coalesce PNG32:tmp.png
+  rm tmp-0.png 
+  rm tmp-1.png
+  convert tmp-*.png +append -filter box -resize 400%x400% $(echo $i | sed 's/source/public\/data/g' | sed 's/psd/png/g')
+  rm tmp-*.png
+done
 
 # Background images
 
