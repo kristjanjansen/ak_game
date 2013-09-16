7b918d7e51b3dd35272e10545aefe7f47027cac7
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-09-16 19:12:30 +0300

    nofps

--- public/js/game.js
+++ public/js/game.js
@@ -1,10 +1,7 @@
 var game = {
 
     "onload" : function () {
-      
-      me.sys.fps = 10;
-      me.sys.useNativeAnimFrame = true;
-        
+            
         if (!me.video.init("screen", 640, 480, true, 'auto')) {
             alert("Your browser does not support HTML5 canvas.");
             return;
