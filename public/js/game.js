b718468a5d5566e755b75f5744f33c268ec69e94
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-09-29 12:25:34 +0300

    fade

--- public/js/game.js
+++ public/js/game.js
@@ -29,9 +29,8 @@ var game = {
       me.state.set(me.state.MENU, new game.TitleScreen());
       me.state.set(me.state.PLAY, new game.PlayScreen());
 		
-	//	  me.entityPool.add("walkable", game.WalkableEntity);
-		
-		
+      me.state.transition("fade", "rgb(0,0,0)", 300);
+				
 		  me.entityPool.add("player", game.PlayerEntity);
 		  me.entityPool.add("bible", game.BibleEntity);
 		  me.entityPool.add("thug", game.ThugEntity);
